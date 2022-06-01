e129393e59784f88a8340994e8224f9d210f7efc
ViktorijaFilipov <viktorija.filipov3141@gmail.com>
2022-06-01 22:04:14 +0200

    add: cypress 10 upgrade; lesson 3

---
+++ LESSON_3/cypress/e2e/intro/testStructure.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+describe('My First Test', () => {
+    before(() => {
+        // runs once before all tests in the block
+    })
+
+    beforeEach(() => {
+        // runs before each test in the block
+    })
+
+    it('Does not do much!', () => {
+        cy.visit('/automation-practice-form');
+        expect(true).to.equal(true)
+    })
+
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+
+    it('Does not do much!', () => {
+        expect(true).to.equal(true)
+    })
+
+    afterEach(() => {
+        // runs after each test in the block
+    })
+
+    after(() => {
+        // runs once after all tests in the block
+    })
+})
